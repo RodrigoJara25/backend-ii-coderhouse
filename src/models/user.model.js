60d650fc8c186dc89fa6ebce1cf1f9e0a85467b1
RodrigoJara25 <r.jara.e7@gmail.com>
2025-11-03 10:31:42 -0500

    feat(user and cart model)

---
+++ src/models/user.model.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const userCollection = "users";
+
+const userSchema = new mongoose.Schema({
+    first_name: {
+        type: String,
+    },
+    last_name: {
+        type: String,
+    },
+    email: {
+        type: String,
+        unique: true,
+    },
+    age: {
+        type: Number,
+    },
+    password: {
+        type: String,
+    },
+    cart: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "carts",
+    },
+    role: {
+        type: String,
+        default: "user",
+    }
+})
+
+const userModel = mongoose.model(userCollection, userSchema);
+
+export default userModel;
