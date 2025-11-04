0f6f29973d61c2791c48506abb11ba79152e0391
RodrigoJara25 <r.jara.e7@gmail.com>
2025-11-03 19:06:42 -0500

    feat(login)

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import envs from './config/envs.config.js';
 import { engine } from 'express-handlebars';
 import { join, __dirname } from "./utils/index.js";
 import cookieParser from 'cookie-parser';
+import passport from 'passport';
+
 import { connectDb } from './config/db.config.js';
+import authRouter from "./routes/session.router.js";
+import viewsRouter from "./routes/views.router.js";
+import initializePassport from './config/passport.config.js';
 
 // settings
 const app = express();
@@ -22,10 +27,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(join(__dirname, "public")));
 app.use(cookieParser());
 
+// passport
+initializePassport();
+app.use(passport.initialize());
+
 // routes
 app.get("/", (req, res) => {
     res.render("home");
 })
+
+app.use("/api/sessions", authRouter);
+app.use("/", viewsRouter);
 
 // nos conectamos a la base de datos
 connectDb(url);
--- src/routes/session.router.js
+++ src/routes/session.router.js
@@ -1,6 +1,49 @@
 import { Router } from "express";
+import passport from "passport";
 
-import userModel from "../models/user.model.js";
+import { generateToken } from "../utils/index.js";
 
 const router = Router();
 
+// registro de usuarios con la estrategia 'register' de passport
+router.post('/register', passport.authenticate('register', {failureRedirect: '/api/sessions/failregister', session: false}), async (req, res) => {
+    res.redirect('/login');
+})
+
+// ruta para manejar fallos en el registro
+router.get('/failregister', (req, res) => {
+    res
+        .status(400)
+        .json({ status: "error", message: "Error al registrar el usuario" });
+});
+
+// login de usuarios con la estrategia 'login' de passport
+router.post('/login', passport.authenticate('login', {failureRedirect: '/api/sessions/faillogin', session: false}), async (req, res) => {
+    // req.user contiene el usuario autenticado
+    const userPayload = {
+        id: req.user._id,
+        first_name: req.user.first_name,
+        last_name: req.user.last_name,
+        email: req.user.email,
+        role: req.user.role
+    };
+    // Genera el token JWT
+    const token = generateToken(userPayload);
+
+    // Guarda el token en una cookie httpOnly
+    res.cookie('authCookie', token, { maxAge: 3600000, httpOnly: true });
+
+    console.log("Sesión inciada con éxito");
+
+    // Redirige al perfil del usuario
+    res.redirect('/profile');
+})
+
+// ruta para manejar fallos en el login
+router.get('/faillogin', (req, res) => {
+    res
+        .status(400)
+        .json({ status: "error", message: "Error al iniciar sesion" });
+});
+
+export default router;
