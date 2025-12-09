a5128427317bf154d4e6fe029bdf156bf3b7f082
RodrigoJara25 <r.jara.e7@gmail.com>
2025-12-09 02:11:27 -0500

    feat(session controller and router)

--- src/controllers/session.controller.js
+++ src/controllers/session.controller.js
@@ -5,6 +5,12 @@ const cartService = new CartService();
 
 export const registerOk = async (req, res) => {
     res.redirect("/login");
+    // respuesta en json
+        // req.user contiene el usuario registrado
+        /*res.status(201).json({
+        status: "success",
+        user: req.user
+    });*/
 };
 
 export const failRegister = (req, res) => {
@@ -14,6 +20,7 @@ export const failRegister = (req, res) => {
 export const loginOk = async (req, res) => {
     const userId = req.user._id.toString();
     await cartService.ensureUserCart(userId);
+    // req.user contiene el usuario autenticado
     const payload = {
         id: req.user._id,
         first_name: req.user.first_name,
@@ -21,13 +28,25 @@ export const loginOk = async (req, res) => {
         email: req.user.email,
         role: req.user.role
     };
-    const token = generateToken(payload);
-    res.cookie(process.env.COOKIE_NAME || "authCookie", token, { maxAge: 3600000, httpOnly: true });
+    const token = generateToken(payload);   // Genera el token JWT
+    res.cookie(process.env.COOKIE_NAME || "authCookie", token, { maxAge: 3600000, httpOnly: true });    // Guarda el token en una cookie httpOnly
+    // Redirige al perfil del usuario
+    console.log("Sesión iniciada con éxito");
     res.redirect("/profile");
+
+    // Respuesta en json
+    /*res.status(200).json({
+        status: "success",
+        message: "Login exitoso",
+        token, // puedes enviar el token si el frontend lo necesita
+        user: userPayload
+    })*/
 };
 
 export const failLogin = (req, res) => {
-    res.status(400).json({ status: "error", message: "Error al iniciar sesión" });
+    res
+        .status(400)
+        .json({ status: "error", message: "Error al iniciar sesión" });
 };
 
 export const current = (req, res) => {
--- src/routes/session.router.js
+++ src/routes/session.router.js
@@ -1,73 +1,32 @@
 import { Router } from "express";
 import passport from "passport";
 
-import { generateToken } from "../utils/index.js";
-import CartService from "../services/cart.service.js";
+import { current, failLogin, failRegister, loginOk, registerOk } from "../controllers/session.controller.js";
 
 const router = Router();
-const cartService = new CartService();
 
 // registro de usuarios con la estrategia 'register' de passport
-router.post('/register', passport.authenticate('register', {failureRedirect: '/api/sessions/failregister', session: false}), async (req, res) => {
-    
-    // respuesta en handlebars
-    res.redirect('/login');
-
-    // respuesta en json
-    // req.user contiene el usuario registrado
-    /*res.status(201).json({
-        status: "success",
-        user: req.user
-    });*/
-})
+router.post('/register', 
+    passport.authenticate('register', {failureRedirect: '/api/sessions/failregister', session: false}), 
+    registerOk
+)
 
 // ruta para manejar fallos en el registro
-router.get('/failregister', (req, res) => {
-    res
-        .status(400)
-        .json({ status: "error", message: "Error al registrar el usuario" });
-});
+router.get('/failregister', failRegister);
 
 // login de usuarios con la estrategia 'login' de passport
-router.post('/login', passport.authenticate('login', {failureRedirect: '/api/sessions/faillogin', session: false}), async (req, res) => {
-    const userId = req.user._id.toString();
-    await cartService.ensureUserCart(userId);
-
-    // req.user contiene el usuario autenticado
-    const userPayload = {
-        id: req.user._id,
-        first_name: req.user.first_name,
-        last_name: req.user.last_name,
-        email: req.user.email,
-        role: req.user.role
-    };
-    
-    const token = generateToken(userPayload);   // Genera el token JWT
-    res.cookie('authCookie', token, { maxAge: 3600000, httpOnly: true });   // Guarda el token en una cookie httpOnly
-
-    // Redirige al perfil del usuario
-    console.log("Sesión inciada con éxito");
-    res.redirect('/profile');
-
-    // Respuesta en json
-    /*res.status(200).json({
-        status: "success",
-        message: "Login exitoso",
-        token, // puedes enviar el token si el frontend lo necesita
-        user: userPayload
-    })*/
-})
+router.post('/login', 
+    passport.authenticate('login', {failureRedirect: '/api/sessions/faillogin', session: false}), 
+    loginOk
+)
 
 // ruta para manejar fallos en el login
-router.get('/faillogin', (req, res) => {
-    res
-        .status(400)
-        .json({ status: "error", message: "Error al iniciar sesion" });
-});
+router.get('/faillogin', failLogin);
 
 // current => ruta para validar al usuario logueado y devolver en una respuesta sus datos
-router.get('/current', passport.authenticate('jwt', { session: false}), (req, res) => {
-    res.json({ status: "success", user: req.user });
-})
+router.get('/current', 
+    passport.authenticate('jwt', { session: false}), 
+    current
+)
 
 export default router;
